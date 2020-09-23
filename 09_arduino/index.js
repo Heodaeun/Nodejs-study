afc9a7e451c9f28a4a898228827cab3547b818bb
Heodaeun <nno3onn@naver.com>
2020-09-23 22:17:00 +0900

    arduino nodejs

--- 09_arduino/index.js
+++ 09_arduino/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(4000, () => { //Start the server, listening on port 4000.
+var server = app.listen(3000, () => { //Start the server, listening on port 4000.
     console.log("Listening to requests on port 4000...");
 })
 
@@ -11,12 +11,12 @@ app.use(express.static('public')); //Send index.html page on GET /
 
 const SerialPort = require('serialport'); 
 const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('COM5'); //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3. See yours on Arduino IDE -> Tools -> Port
+const port = new SerialPort('COM5'); //Connect serial port to port COM5. Because my Arduino Board is connected on port COM5. See yours on Arduino IDE -> Tools -> Port
 const parser = port.pipe(new Readline({delimiter: '\r\n'})); //Read the line only when new line comes.
 parser.on('data', (temp) => { //Read data
     console.log(temp);
     var today = new Date();
-    io.sockets.emit('temp', {date: today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear(), time: (today.getHours())+":"+(today.getMinutes()), temp:temp}); //emit the datd i.e. {date, time, temp} to all the connected clients.
+    io.sockets.emit('temp', {date: today.getDate()+"-"+(today.getMonth()+1)+"-"+today.getFullYear(), time: (today.getHours())+":"+(today.getMinutes()), temp:temp}); //emit the datd i.e. {date, time, temp} to all the connected clients.
 });
 
 io.on('connection', (socket) => {
