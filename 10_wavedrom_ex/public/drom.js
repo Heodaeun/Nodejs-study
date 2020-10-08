4e3d9f7c911acc2fc2b0ba1ed91afd10deb2fdaf
Heodaeun <nno3onn@naver.com>
2020-10-08 23:51:28 +0900

    update

--- 10_wavedrom_ex/public/drom.js
+++ 10_wavedrom_ex/public/drom.js
@@ -2,23 +2,36 @@
 // $("#wave").text = drom;
 // console.log('drom:' ,drom);
 
+// var page = document.getElementById('page');
+// page.innerText = "hi";
+
 // var hw = document.getElementById('hw');
 // hw.addEventListener('click', function(){
-//     alert('Hello world');
+//     page.innerText = "bye";
 // });
 
-require('../index.js');
+
+// require('../index.js');
 
 // const express = require('express');
 // const app = express();
 // const fs = require('fs');
+// read json file
+// const fs = fs();
 
-// // read json file
+
+console.log('hi');
+// var drom = document.getElementById('drom');
+// drom.innerText = '{ signal : [{ name: "clk",  wave: "p......" }]}';
+
+
+
+
 // var dataBuffer = fs.readFileSync('WaveDrom.json');
 // var dataJSON = dataBuffer.toString();
 // var drom = JSON.parse(dataJSON);
 
-// // console.log(drom);
+// console.log('drom.js: ', drom);
 // var ref = document.getElementsByTagName('script')[0],
 // script = document.createElement('script');
 
