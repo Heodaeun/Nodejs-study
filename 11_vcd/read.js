9d86a35b42c31bf36085ee7f9887cb5f4918b922
Heodaeun <nno3onn@naver.com>
2020-10-17 01:52:35 +0900

    complete parsing vcd file

--- 11_vcd/read.js
+++ 11_vcd/read.js
@@ -19,7 +19,7 @@ for (var line = 0; line < lines.length; line++) {
         var id = var_line[4];
         
         nameJSON[char] = id;
-        WaveJSON['signal'].push({"name" : id});
+        WaveJSON['signal'].push({"name" : id, "wave" : ""});
 
     // 2. $dumpvars (초기값)
     }else if(this_line == '$dumpvars'){
@@ -29,14 +29,8 @@ for (var line = 0; line < lines.length; line++) {
         if(this_line == "$end"){    // $end가 나오기 전까지 계속 반복
             check = false;
         }else{
-            var find_name = this_line.slice(-1);
-            
-            for(i in WaveJSON.signal){
-                console.log(i.name);
-                if(find_name == WaveJSON.signal[i].name){
-                    console.log(this_line.slice(-1));
-                }
-            }
+         
+            insert_wave(0);
         }
 
     // 3. #number
@@ -44,10 +38,22 @@ for (var line = 0; line < lines.length; line++) {
         check2 = true;
 
     }else if(check2 == true){
-        if(this_line == "$end"){
-            check2 == false;
-        }else{
+        if(this_line.slice(0,1) != "#"){
+            insert_wave(1);
+        }
+    }
+}
 
+
+function insert_wave(check){
+    var find_name = this_line.slice(-1);
+    for(i in WaveJSON.signal){  //i : n번째
+        j = WaveJSON.signal[i]; //j : i번째 JSON Data
+        if(j.name == nameJSON[find_name]){
+            var waveData = this_line.slice(0, -1);
+            waveData = waveData.split(" ").join("");    //공백 제거
+            check = 0 ? WaveJSON.signal[i].wave = waveData
+                     : WaveJSON.signal[i].wave += waveData;
         }
     }
 }
